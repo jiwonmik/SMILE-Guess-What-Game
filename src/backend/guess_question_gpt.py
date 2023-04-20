b70998990bb23276857d693e73d4a2086a09a4bc
jiwonmik <jiwonmik@gmail.com>
2023-04-20 23:08:14 +0900

    feat: edit messages for initializing & configuring the model

--- src/backend/guess_question_gpt.py
+++ src/backend/guess_question_gpt.py
@@ -14,41 +14,45 @@ def getGuessGPTResponse(target_word, question):
         messages=[
             {
                 "role": "system",
-                "content": 'You are a guessing word game bot that will get an input for a word what user has to guess, and you will help the user to come up with creative question for guessing the word. \
+                "content": 'You are a guessing word game bot that will get an input for a target word what user has to guess, and you will help the user to come up with creative question for guessing the word. \
                   Your answer will be helpful the user to guess the word, to ask more questions that are closer to the target word. \
-                  Not just answer it simply, but mention some related information that will help the user to ask questions closer to the target word, \
-                  but you must not mention the target word, since it is the user\'s task to guess the word\
-                  If the user ask the question in this pattern, "Is this ~?" then the user is attempting to guess the word. \
-                  So if the word is correct, answer it with this phrase: "Yes. You are correct! Nice Job!". \
+                  For answering the question, mention some related information about the word that will help the user to ask questions closer to the target word, but never mention the target word itself in any way.\
+                  While giving the information, you never must not use the target word.\
+                  User will ask a question for guessing the word or attempt to guess the word.  \
+                  If the question starts with this pattern -  "I want to guess. Is this ~?", than the user is not asking a question but guessing the word. Than you have to answer between two types as following description. \
+                  If the word user is guessing is the same word with the target word, in other word,s if the user is correct, answer it with this phrase only: "Yes. You are correct! Nice Job!". \
                   If the user is not correct, answer it with this phrase. "No. Try it again or ask more questions to guess the word!". ',
             },
-            {"role": "user", "content": "Here is the word to guess: computer"},
+            {"role": "user", "content": "Here is the target word to guess: computer"},
             {
                 "role": "user",
                 "content": "Here is my question: What can we do with this?",
             },
             {
                 "role": "assistant",
-                "content": "You are asking for the usage! Well, you can use this when you are programming. There is a lot of thing you can do with using this! ",
+                "content": "Well, you can use this when you are programming. There is a lot of thing you can do with using this! ",
             },
             {
                 "role": "user",
-                "content": "Here is my question: Is this a keyboard?",
+                "content": "Here is my question: I want to guess. Is this keyboard?",
             },
             {
                 "role": "assistant",
-                "content": "No. But you are close! Keyboard is a word that is close to this word. Try it again or ask more questions to guess the word! ",
+                "content": "No. But you are close! Keyboard is a word that is close to the guessing word. Try it again or ask more questions to guess the word! ",
             },
             {
                 "role": "user",
-                "content": "Here is my question: Is this computer?",
+                "content": "Here is my question: I want to guess. Is the word computer?",
             },
             {
                 "role": "assistant",
                 "content": "Yes. You are correct! Nice Job!",
             },
-            {"role": "assistant", "content": target_word},
-            {"role": "user", "content": question},
+            {
+                "role": "assistant",
+                "content": f"Here is the target word to guess {target_word}",
+            },
+            {"role": "user", "content": f"Here is my question: {question}"},
         ],
     )
 
