bbefd08c2701f32b17db1c6e0665cfca46755bb9
jiwonmik <jiwonmik@gmail.com>
2023-04-23 11:51:15 +0900

    fix: edit messages for the GPT Model 1. Problem: The model keeps mentioning the word which should'nt be, since it is the user's task to guess the word. 2. Edited the message: If you have to mention the word, you must call it as "this" or "this word."

--- src/backend/guess_question_gpt.py
+++ src/backend/guess_question_gpt.py
@@ -8,22 +8,22 @@ openai.api_key = API_KEY
 model_id = "gpt-3.5-turbo"
 
 
-def getGuessGPTResponse(target_word, question):
+def getGuessGPTResponse(word, question):
     response = openai.ChatCompletion.create(
         model=model_id,
         messages=[
             {
                 "role": "system",
-                "content": 'You are a guessing word game bot that will get an input for a target word what user has to guess, and you will help the user to come up with creative question for guessing the word. \
-                  Your answer will be helpful the user to guess the word, to ask more questions that are closer to the target word. \
-                  For answering the question, mention some related information about the word that will help the user to ask questions closer to the target word, but never mention the target word itself in any way.\
-                  While giving the information, you never must not use the target word.\
+                "content": 'You are a guessing word game bot that will get an input for a word what user has to guess, and you will help the user to come up with creative question for guessing the word. \
+                  Your answer will be helpful the user to guess the word, to ask more questions that are closer to the word. \
+                  For answering the question, mention some related information about the word that will help the user to ask questions closer to the word, but never mention the word given as an input for a word in any way.\
+                  While giving the information, you never must not use the word. If you have to mention the word, you must call it as "this" or "this word."\
                   User will ask a question for guessing the word or attempt to guess the word.  \
                   If the question starts with this pattern -  "I want to guess. Is this ~?", than the user is not asking a question but guessing the word. Than you have to answer between two types as following description. \
-                  If the word user is guessing is the same word with the target word, in other word,s if the user is correct, answer it with this phrase only: "Yes. You are correct! Nice Job!". \
+                  If the word user is guessing is the same word with the word, in other word,s if the user is correct, answer it with this phrase only: "Yes. You are correct! Nice Job!". \
                   If the user is not correct, answer it with this phrase. "No. Try it again or ask more questions to guess the word!". ',
             },
-            {"role": "user", "content": "Here is the target word to guess: computer"},
+            {"role": "assistant", "content": "Here is the word to guess: computer"},
             {
                 "role": "user",
                 "content": "Here is my question: What can we do with this?",
@@ -50,7 +50,7 @@ def getGuessGPTResponse(target_word, question):
             },
             {
                 "role": "assistant",
-                "content": f"Here is the target word to guess {target_word}",
+                "content": f"Here is the word to guess : {word}",
             },
             {"role": "user", "content": f"Here is my question: {question}"},
         ],
