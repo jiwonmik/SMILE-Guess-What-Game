574b4821b47de04338816588027c15b86a9b63b6
jiwonmik <jiwonmik@gmail.com>
2023-05-01 18:30:33 +0900

    fix: edit messages 1. edit messages which included the guess word for the answer - found what was the problem

--- src/backend/guess_question_gpt.py
+++ src/backend/guess_question_gpt.py
@@ -15,10 +15,10 @@ def getGuessGPTResponse(word, question):
         messages=[
             {
                 "role": "system",
-                "content": 'You are a guessing word game system. that you will help the user to come up with creative question about the guess word, and the final goal of the user is to guess the word correctly. \
+                "content": 'You are a Guess Word Game. You will help the user to come up with creative question about the guess word, and the final goal of the user is to guess the word correctly. \
                   Your answer will be helpful for the user to guess the word. Help the user to ask more questions that are closer to the word. \
-                  When you answer, give some related information about the guess word that will help the user to ask questions closer to the word, but you have to never mention the guess word given in any way, even in other tense of the word.\
-                  While giving the information, you must never use the guess word. If you have to mention the guess word, you must call it as "this" or "this word."\
+                  When you answer, give some related information that will help the user to ask questions closer to the word, but you are not allowed to mention the guess word in any way, since the guess word is secrete.\
+                  While giving the information, you must never mention the guess word. If you have to, you must reference it as "this" or "this word."\
                   User\'s question will be either a question or an attempt to guess the word.  \
                   If the user attempt to guess the word, the pattern of the phrase will be like this: "I want to guess. Is the word". The user is not asking a question but guessing the word. \
                   Firstly, if the user is correct, answer with this phrase only: "Congratulations! You are correct! Nice Job!". \
@@ -31,7 +31,7 @@ def getGuessGPTResponse(word, question):
             },
             {
                 "role": "assistant",
-                "content": "Computers are playing a vital role in almost every field and making our day-to-day tasks more manageable. Computers were only used to perform complex numerical calculations in a previous time, but they have reached too far and now perform many different roles. They are now performing diverse set functions from complicated calculations to generating business reports, bill generation to education, programming or development to entertainment, etc.",
+                "content": "This are playing a vital role in almost every field and making our day-to-day tasks more manageable. This was only used to perform complex numerical calculations in a previous time, but they have reached too far and now perform many different roles. They are now performing diverse set functions from complicated calculations to generating business reports, bill generation to education, programming or development to entertainment, etc.",
             },
             {
                 "role": "user",
@@ -39,7 +39,7 @@ def getGuessGPTResponse(word, question):
             },
             {
                 "role": "assistant",
-                "content": "No. But you are close! Keyboard is a word that is close to the guessing word. Try it again or ask more questions to guess the word! ",
+                "content": "No. But you are close! Try it again or ask more questions to guess the word! ",
             },
             {
                 "role": "user",
